640505ee9ce512ad7aed91522801d99625c86101
Hisanari Otsu <hi2p.perim@gmail.com>
2018-10-25 10:11:21 +0200

    wip. asset management

--- lm/include/lm/logger.h
+++ lm/include/lm/logger.h
@@ -74,24 +74,29 @@ LM_NAMESPACE_END(LM_NAMESPACE)
 /*!
     \brief Log error message.
 */
-#define LM_LOG_ERROR(message, ...) LM_NAMESPACE::log::detail::log(LM_NAMESPACE::log::detail::LogLevel::Err, __FILE__, __LINE__, message)
+#define LM_LOG_ERROR(message, ...) LM_NAMESPACE::log::detail::log( \
+    LM_NAMESPACE::log::detail::LogLevel::Err, __FILE__, __LINE__, message)
 
 /*!
     \brief Log warning message.
 */
-#define LM_LOG_WARN(message, ...) LM_NAMESPACE::log::detail::log(LM_NAMESPACE::log::detail::LogLevel::Warn, __FILE__, __LINE__, message)
+#define LM_LOG_WARN(message, ...) LM_NAMESPACE::log::detail::log( \
+    LM_NAMESPACE::log::detail::LogLevel::Warn, __FILE__, __LINE__, message)
 
 /*!
     \brief Log info message.
 */
-#define LM_LOG_INFO(message, ...) LM_NAMESPACE::log::detail::log(LM_NAMESPACE::log::detail::LogLevel::Err, __FILE__, __LINE__, message)
+#define LM_LOG_INFO(message, ...) LM_NAMESPACE::log::detail::log( \
+    LM_NAMESPACE::log::detail::LogLevel::Err, __FILE__, __LINE__, message)
 
 /*!
     \brief Log debug message.
 */
-#define LM_LOG_DEBUG(message, ...) LM_NAMESPACE::log::detail::log(LM_NAMESPACE::log::detail::LogLevel::Debug, __FILE__, __LINE__, message)
+#define LM_LOG_DEBUG(message, ...) LM_NAMESPACE::log::detail::log( \
+    LM_NAMESPACE::log::detail::LogLevel::Debug, __FILE__, __LINE__, message)
 
 /*!
     \brief Adds an indentation in the current scope.
 */
-#define LM_LOG_INDENTER() LM_NAMESPACE::log::detail::LogIndenter LM_TOKENPASTE2(logIndenter_, __LINE__)
+#define LM_LOG_INDENTER() LM_NAMESPACE::log::detail::LogIndenter \
+    LM_TOKENPASTE2(logIndenter_, __LINE__)
