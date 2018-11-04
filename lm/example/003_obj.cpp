55cb6c1a8f7694c9526facf93d02905e3b04285b
Hisanari Otsu <hi2p.perim@gmail.com>
2018-11-04 20:51:52 +0100

    small fix

--- lm/example/003_obj.cpp
+++ lm/example/003_obj.cpp
@@ -43,7 +43,6 @@ int main(int argc, char** argv) {
 
     // OBJ model
     lm::asset("obj1", "model::wavefrontobj", {{"path", opt["obj"]}});
-    #pragma endregion
     
     // ------------------------------------------------------------------------
 
