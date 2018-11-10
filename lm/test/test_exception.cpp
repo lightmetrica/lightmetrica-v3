564b6b532c6e929fac4ed98646cd36c15b559126
Hisanari Otsu <hi2p.perim@gmail.com>
2018-11-10 11:28:12 +0100

    added some tests for test_exception

--- lm/test/test_exception.cpp
+++ lm/test/test_exception.cpp
@@ -16,7 +16,6 @@ LM_NAMESPACE_BEGIN(LM_TEST_NAMESPACE)
 
 TEST_CASE("Exception") {
     lm::log::ScopedInit log_;
-    lm::exception::ScopedInit ex_;
 
     const auto Check = [](const std::function<void()>& func) -> std::string {
         try { func(); }
@@ -28,57 +27,119 @@ TEST_CASE("Exception") {
 
     SUBCASE("Supported") {
         SUBCASE("Multiply inftinity and zero") {
-            const auto code = Check([]() {
+            const auto f = []() {
                 // We put volatile keyword to supress optimization
                 const volatile double z = 0;
                 const volatile double t = std::numeric_limits<double>::infinity() * z;
                 LM_UNUSED(t);
-            });
-            CHECK(code == "EXCEPTION_FLT_INVALID_OPERATION");
+            };
+            SUBCASE("Enabled") {
+                lm::exception::ScopedInit ex_;
+                CHECK(Check(f) == "EXCEPTION_FLT_INVALID_OPERATION");
+            }
+            SUBCASE("Disabled") {
+                CHECK(Check(f).empty());
+            }
         }
 
         SUBCASE("Divide zero by zero") {
-            const auto code = Check([]() {
+            const auto f = []() {
                 const volatile double z = 0;
                 const volatile double t = 0 / z;
                 LM_UNUSED(t);
-            });
-            CHECK(code == "EXCEPTION_FLT_INVALID_OPERATION");
+            };
+            SUBCASE("Enabled") {
+                lm::exception::ScopedInit ex_;
+                CHECK(Check(f) == "EXCEPTION_FLT_INVALID_OPERATION");
+            }
+            SUBCASE("Disabled") {
+                CHECK(Check(f).empty());
+            }
         }
 
         SUBCASE("Divide by zero") {
-            const auto code = Check([]() {
+            const auto f = []() {
                 const volatile double z = 0;
                 const volatile double t = 1.0 / z;
                 LM_UNUSED(t);
-            });
-            CHECK(code == "EXCEPTION_FLT_DIVIDE_BY_ZERO");
+            };
+            SUBCASE("Enabled") {
+                lm::exception::ScopedInit ex_;
+                CHECK(Check(f) == "EXCEPTION_FLT_DIVIDE_BY_ZERO");
+            }
+            SUBCASE("Disabled") {
+                CHECK(Check(f).empty());
+            }
         }
 
         SUBCASE("Square root of -1") {
-            const auto code = Check([]() {
+            const auto f = []() {
                 const volatile double t = std::sqrt(-1);
                 LM_UNUSED(t);
-            });
-            CHECK(code == "EXCEPTION_FLT_INVALID_OPERATION");
+            };
+            SUBCASE("Enabled") {
+                lm::exception::ScopedInit ex_;
+                CHECK(Check(f) == "EXCEPTION_FLT_INVALID_OPERATION");
+            }
+            SUBCASE("Disabled") {
+                CHECK(Check(f).empty());
+            }
         }
 
         #if 0
         // Temporarily ignored this test case due to the bug in VS2017
         // https://developercommunity.visualstudio.com/content/problem/155064/stdnumeric-limitssignaling-nan-returns-quiet-nan.html
         SUBCASE("Signaling NaN") {
-            const auto code = Check([]() {
+            const auto f = []() {
                 const volatile double one = 1.0;
                 const volatile double nan = std::numeric_limits<double>::signaling_NaN();
                 const volatile double t = one * nan;
                 LM_UNUSED(t);
-            });
-            CHECK(code == "EXCEPTION_FLT_INVALID_OPERATION");
+            };
+            SUBCASE("Enabled") {
+                lm::exception::ScopedInit ex_;
+                CHECK(Check(f) == "EXCEPTION_FLT_INVALID_OPERATION");
+            }
+            SUBCASE("Disabled") {
+                CHECK(Check(f).empty());
+            }
         }
         #endif
     }
 
     // ------------------------------------------------------------------------
+
+    SUBCASE("Unsupported") {
+        lm::exception::ScopedInit ex_;
+
+        SUBCASE("Denormal") {
+            // 4.940656e-324 : denormal
+            const double t = 4.940656e-324;
+            CHECK(std::fpclassify(t) == FP_SUBNORMAL);
+        }
+
+        SUBCASE("Below denormal") {
+            // 4.940656e-325 : below representable number by denormal -> clamped to zero
+            const double t = 4.940656e-325;
+            CHECK(std::fpclassify(t) == FP_ZERO);
+        }
+
+        SUBCASE("Inexact 1") {
+            const auto code = Check([]() {
+                const volatile double t = 2.0 / 3.0;
+                LM_UNUSED(t);
+            });
+            CHECK(code.empty());
+        }
+
+        SUBCASE("Inexact 2") {
+            const auto code = Check([]() {
+                const volatile double t = std::log(1.1);
+                LM_UNUSED(t);
+            });
+            CHECK(code.empty());
+        }
+    }
 }
 
 LM_NAMESPACE_END(LM_TEST_NAMESPACE)
