be6ba5dd8f06782b62420113d4a275dbdddb60a3
Hisanari Otsu <hi2p.perim@gmail.com>
2019-01-02 13:31:32 +0100

    writing documentation

--- include/lm/user.h
+++ include/lm/user.h
@@ -18,7 +18,7 @@ LM_NAMESPACE_BEGIN(LM_NAMESPACE)
 // ----------------------------------------------------------------------------
 
 /*!
-    \brief Initializes the renderer.
+    \brief Initialize the renderer.
     \param prop Properties for configuration.
     \see `example/blank.cpp`
 
@@ -32,30 +32,77 @@ LM_PUBLIC_API void init(const Json& prop = {});
 
 /*!
     \brief Shutdown the renderer.
+
+    \rst
+    You don't need to explicitly call this function at the end of your application.
+    You want to call this function if you want explicit shutdown of the renderer.
+    If you want to use scoped initialization/shutdown,
+    consider to use :class:`lm::ScopedInit`.
+    \endrst
 */
 LM_PUBLIC_API void shutdown();
 
 /*!
     \brief Create an asset.
     \param name Identifier of the asset.
-    \param implKey Name of aset to create.
+    \param implKey Name of the asset to create.
     \param prop Properties for configuration.
+    \see `example/blank.cpp`
+
+    \rst
+    Asset represents a basic component of the scene object like meshes or materials.
+    We use assets as building blocks to construct the scene of the framework.
+    This function creates an instance of an asset and register it to the framework
+    by a given identifier. ``name`` is used as a reference by other APIs.
+    ``implKey`` has a format of ``<asset type>::<implementation>``.
+    If ``name`` is already registered or ``implKey`` is missing,
+    the framework will generate corresponding error messages
+    through logger system as well as runtime error exception.
+    \endrst
 */
 LM_PUBLIC_API void asset(const std::string& name,
     const std::string& implKey, const Json& prop);
 
 /*!
     \brief Get an asset by name.
+    \param name Identifier of the asset.
+    \return Pointer to the registered asset. ``nullptr`` if not registered.
+
+    \rst
+    If the asset specified by ``name`` is not registered,
+    the function will generate an error on logger and return nullptr.
+    \endrst
 */
 LM_PUBLIC_API Component* getAsset(const std::string& name);
 
 /*!
     \brief Create a primitive and add it to the scene.
+    \param transform Transformation matrix.
+    \param prop Properties for configuration.
+    \see `example/quad.cpp`
+
+    \rst
+    This function creates a primitive and registers to the framework.
+    A primitive is a scene object associating the assets such as
+    meshes or materials. The coordinates of the object is
+    speficied by a 4x4 transformation matrix.
+    We can use the same assets to define different primitives
+    with different transformations.
+    \endrst
 */
 LM_PUBLIC_API void primitive(Mat4 transform, const Json& prop);
 
 /*!
     \brief Create primitives from a model.
+    \param transform Transformation matrix.
+    \param modelName Identifier of `model` asset.
+    \see `example/raycast.cpp`
+
+    \rst
+    A ``model`` asset internally creates a set of meshes and materials.
+    This function generates a set of primitives from a model asset.
+    The transformation is applied to all primitives to be generated.
+    \endrst
 */
 LM_PUBLIC_API void primitives(Mat4 transform, const std::string& modelName);
 
