d3adfdde7c41f3f484320726d3a06a14fd35cb0e
Hisanari Otsu <hi2p.perim@gmail.com>
2019-02-13 09:30:55 +0100

    wip. adding new tests

---
+++ test/test_debugio.cpp
@@ -0,0 +1,19 @@
+/*
+    Lightmetrica - Copyright (c) 2019 Hisanari Otsu
+    Distributed under MIT license. See LICENSE file for details.
+*/
+
+#include <pch.h>
+#include "test_common.h"
+#include <lm/debugio.h>
+
+LM_NAMESPACE_BEGIN(LM_TEST_NAMESPACE)
+
+TEST_CASE("Debugio") {
+    lm::log::ScopedInit log_;
+    
+
+
+}
+
+LM_NAMESPACE_END(LM_TEST_NAMESPACE)
