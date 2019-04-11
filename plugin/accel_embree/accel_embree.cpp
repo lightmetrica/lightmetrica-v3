a4a8ba84eb2f3389a28139b8fa063ab436f803c2
Hisanari Otsu <hi2p.perim@gmail.com>
2019-04-11 18:59:48 +0200

    wip. embree integration

---
+++ plugin/accel_embree/accel_embree.cpp
@@ -0,0 +1,10 @@
+/*
+    Lightmetrica - Copyright (c) 2019 Hisanari Otsu
+    Distributed under MIT license. See LICENSE file for details.
+*/
+
+#include <lm/accel.h>
+#include <lm/scene.h>
+#include <lm/exception.h>
+
+
