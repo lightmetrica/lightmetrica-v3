9542e77cd348052abf06e9ac6f9819d2cc04045a
Hisanari Otsu <hi2p.perim@gmail.com>
2020-01-06 11:35:07 +0100

    removed previous impl

--- src/model/model_wavefrontobj.cpp
+++ src/model/model_wavefrontobj.cpp
@@ -14,8 +14,6 @@
 #include <lm/light.h>
 #include <lm/surface.h>
 
-#define MODEL_WAVEFRONTOBJ_NEW_MIXTURE_MATERIAL 1
-
 LM_NAMESPACE_BEGIN(LM_NAMESPACE)
 
 using namespace objloader;
@@ -82,8 +80,6 @@ public:
                 assets_map_[mesh_name] = int(assets_.size());
                 assets_.push_back(std::move(mesh));
 
-                // --------------------------------------------------------------------------------
-
                 // Create area light if Ke > 0
                 int light_index = -1;
                 if (glm::compMax(m.Ke) > 0_f) {
@@ -101,15 +97,11 @@ public:
                     assets_.push_back(std::move(light));
                 }
 
-                // --------------------------------------------------------------------------------
-
                 // Create mesh group
                 groups_.push_back({ assets_map_[mesh_name], assets_map_[m.name], light_index });
 
                 return true;
             },
-
-            // ------------------------------------------------------------------------------------
 
             // Process material
             [&](const MTLMatParams& m) -> bool {
@@ -125,8 +117,6 @@ public:
                     assets_.push_back(std::move(mat));
                     return true;
                 }
-
-                // --------------------------------------------------------------------------------
 
                 // Load texture
                 std::string mapKd_loc;
@@ -152,9 +142,6 @@ public:
                     mapKd_loc = make_loc(id);
                 }
 
-                // --------------------------------------------------------------------------------
-
-#if MODEL_WAVEFRONTOBJ_NEW_MIXTURE_MATERIAL
                 // Load material
                 Ptr<Material> mat;
                 if (m.illum == 7) {
@@ -187,23 +174,6 @@ public:
                 }
                 assets_map_[m.name] = int(assets_.size());
                 assets_.push_back(std::move(mat));
-#else
-
-
-                // Load material
-                auto mat = comp::create<Material>(
-                    "material::wavefrontobj", make_loc(m.name),
-                    json::merge(prop, {
-                        {"matparams_", (const MTLMatParams*)&m},
-                        {"mapKd_", mapKd_loc}
-                    }
-                ));
-                if (!mat) {
-                    return false;
-                }
-                assets_map_[m.name] = int(assets_.size());
-                assets_.push_back(std::move(mat));
-#endif
 
                 return true;
             });
@@ -481,227 +451,5 @@ public:
 };
 
 LM_COMP_REG_IMPL(Material_WavefrontObj_Mixture, "material::wavefrontobj_mixture");
-
-// ------------------------------------------------------------------------------------------------
-
-class Material_WavefrontObj final : public Material {
-private:
-    // Material parameters of MLT file
-    MTLMatParams objmat_;
-
-    // Underlying material components
-    std::vector<Component::Ptr<Material>> materials_;
-    std::unordered_map<std::string, int> type_to_index_map_;
-
-    // Component indices
-    int diffuse_ = -1;
-    int glossy_ = -1;
-    int glass_ = -1;
-    int mirror_ = -1;
-    int mask_ = -1;
-
-    // Texture for the alpha mask
-    Texture* mask_tex_ = nullptr;
-
-public:
-    LM_SERIALIZE_IMPL(ar) {
-        ar(objmat_, materials_, type_to_index_map_,
-           diffuse_, glossy_, glass_, mirror_, mask_,
-           mask_tex_);
-    }
-
-    virtual Component* underlying(const std::string& name) const override {
-        if (auto it = type_to_index_map_.find(name); it != type_to_index_map_.end()) {
-            return materials_.at(it->second).get();
-        }
-        return nullptr;
-    }
-
-    virtual void foreach_underlying(const ComponentVisitor& visit) override {
-        comp::visit(visit, mask_tex_);
-        for (auto& material : materials_) {
-            comp::visit(visit, material);
-        }
-    }
-
-    virtual Json underlying_value(const std::string& query) const override {
-        LM_UNUSED(query);
-        return {
-            { "name", objmat_.name },
-            { "illum", objmat_.illum },
-            { "Kd", objmat_.Kd },
-            { "Ks", objmat_.Ks },
-            { "Ke", objmat_.Ke },
-            { "mapKd", objmat_.mapKd },
-            { "Ni", objmat_.Ni },
-            { "Ns", objmat_.Ns },
-            { "an", objmat_.an },
-        };
-    }
-
-private:
-    int add_material(const std::string& name, const std::string& type, const Json& prop) {
-        auto p = comp::create<Material>(name, make_loc(type), prop);
-        if (!p) {
-            return -1;
-        }
-        int index = int(materials_.size());
-        materials_.push_back(std::move(p));
-        type_to_index_map_[type] = index;
-        return index;
-    }
-
-public:
-    virtual void construct(const Json& prop) override {
-        objmat_ = *prop["matparams_"].get<const MTLMatParams*>();
-        objmat_.mapKd = prop["mapKd_"];
-
-        // Make parent component as a parent for the newly created components
-        if (objmat_.illum == 7) {
-            // Glass material
-            const auto glass_material_name = json::value<std::string>(prop, "glass", "material::glass");
-            glass_ = add_material(glass_material_name, "glass", {
-                {"Ni", objmat_.Ni}
-            });
-            if (glass_ < 0) {
-                LM_THROW_EXCEPTION_DEFAULT(Error::InvalidArgument);
-            }
-        }
-        else if (objmat_.illum == 5) {
-            // Mirror material
-            const auto mirror_material_name = json::value<std::string>(prop, "mirror", "material::mirror");
-            mirror_ = add_material(mirror_material_name, "mirror", {});
-            if (mirror_ < 0) {
-                LM_THROW_EXCEPTION_DEFAULT(Error::InvalidArgument);
-            }
-        }
-        else {
-            // Diffuse material
-            const auto diffuse_material_name = json::value<std::string>(prop, "diffuse", "material::diffuse");
-            Json matprop{ {"Kd", objmat_.Kd} };
-            if (!objmat_.mapKd.empty()) {
-                matprop["mapKd"] = objmat_.mapKd;
-            }
-            diffuse_ = add_material(diffuse_material_name, "diffuse", matprop);
-            if (diffuse_ < 0) {
-                LM_THROW_EXCEPTION_DEFAULT(Error::InvalidArgument);
-            }
-
-            // Glossy material
-            const auto glossy_material_name = json::value<std::string>(prop, "glossy", "material::glossy");
-            const auto r = 2_f / (2_f + objmat_.Ns);
-            const auto as = math::safe_sqrt(1_f - objmat_.an * .9_f);
-            glossy_ = add_material(glossy_material_name, "glossy", {
-                {"Ks", objmat_.Ks},
-                {"ax", std::max(1e-3_f, r / as)},
-                {"ay", std::max(1e-3_f, r * as)}
-            });
-            if (glossy_ < 0) {
-                LM_THROW_EXCEPTION_DEFAULT(Error::InvalidArgument);
-            }
-
-            // Mask texture
-            if (!objmat_.mapKd.empty()) {
-                auto* texture = lm::comp::get<Texture>(objmat_.mapKd);
-                if (texture->has_alpha()) {
-                    mask_tex_ = texture;
-                    mask_ = add_material("material::mask", "mask", {});
-                    if (mask_ < 0) {
-                        LM_THROW_EXCEPTION_DEFAULT(Error::InvalidArgument);
-                    }
-                }
-            }
-        }
-    }
-
-    virtual bool is_specular(const PointGeometry& geom, int comp) const override {
-        return materials_.at(comp)->is_specular(geom, SurfaceComp::DontCare);
-    }
-
-    virtual std::optional<MaterialDirectionSample> sample(Rng& rng, const PointGeometry& geom, Vec3 wi) const override {
-        // Select component
-        const auto [comp, weight] = [&]() -> std::tuple<int, Float> {
-            // Glass
-            if (glass_ >= 0) {
-                return { glass_, 1_f };
-            }
-
-            // Mirror
-            if (mirror_ >= 0) {
-                return { mirror_, 1_f };
-            }
-
-            // Diffuse or glossy or mask
-            const auto wd = diffuse_selection_weight(geom);
-            if (rng.u() < wd) {
-                if (mask_tex_ && rng.u() > mask_tex_->eval_alpha(geom.t)) {
-                    return { mask_, 1_f/wd };
-                }
-                else {
-                    return { diffuse_, 1_f/wd };
-                }
-            }
-            return { glossy_, 1_f/(1_f-wd) };
-        }();
-
-        // Sample a ray
-        auto s = materials_.at(comp)->sample(rng, geom, wi);
-        if (!s) {
-            return {};
-        }
-        return MaterialDirectionSample{
-            s->wo,
-            comp,
-            s->weight * weight
-        };
-    }
-
-    virtual std::optional<Vec3> sample_direction_given_comp(Rng& rng, const PointGeometry& geom, int comp, Vec3 wi) const override {
-        return materials_.at(comp)->sample_direction_given_comp(rng, geom, SurfaceComp::DontCare, wi);
-    }
-
-    virtual std::optional<Vec3> reflectance(const PointGeometry& geom, int comp) const override {
-        if (comp >= 0) {
-            return materials_.at(comp)->reflectance(geom, SurfaceComp::DontCare);
-        }
-        if (diffuse_ >= 0) {
-            return materials_.at(diffuse_)->reflectance(geom, SurfaceComp::DontCare);
-        }
-        return {};
-    }
-
-    virtual Float pdf(const PointGeometry& geom, int comp, Vec3 wi, Vec3 wo) const override {
-        return materials_.at(comp)->pdf(geom, SurfaceComp::DontCare, wi, wo);
-    }
-
-    virtual Float pdf_comp(const PointGeometry& geom, int comp, Vec3) const override {
-        if (comp == glass_ || comp == mirror_) {
-            return 1_f;
-        }
-        const auto wd = diffuse_selection_weight(geom);
-        return comp == glossy_ ? (1_f - wd) : wd;
-    }
-
-    virtual Vec3 eval(const PointGeometry& geom, int comp, Vec3 wi, Vec3 wo) const override {
-        return materials_.at(comp)->eval(geom, SurfaceComp::DontCare, wi, wo);
-    }
-
-private:
-    Float diffuse_selection_weight(const PointGeometry& geom) const {
-        const auto* D = materials_.at(diffuse_).get();
-        const auto* G = materials_.at(glossy_).get();
-        const auto wd = [&]() {
-            const auto wd = glm::compMax(*D->reflectance(geom, SurfaceComp::DontCare));
-            const auto ws = glm::compMax(*G->reflectance(geom, SurfaceComp::DontCare));
-            if (wd == 0_f && ws == 0_f) {
-                return 1_f;
-            }
-            return wd / (wd + ws);
-        }();
-        return wd;
-    }
-};
-
-LM_COMP_REG_IMPL(Material_WavefrontObj, "material::wavefrontobj");
 
 LM_NAMESPACE_END(LM_NAMESPACE)
