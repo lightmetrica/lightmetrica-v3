1c252bbfa20df4968577648990f9a3f4f787b086
Hisanari Otsu <hi2p.perim@gmail.com>
2020-02-15 17:53:04 +0100

    removed an example

--- functest/renderer_ao.cpp
+++ functest/renderer_ao.cpp
@@ -18,33 +18,31 @@ public:
     virtual void construct(const Json& prop) override {
         scene_ = json::comp_ref<Scene>(prop, "scene");
         film_ = json::comp_ref<Film>(prop, "output");
+        scene_->camera()->set_aspect_ratio(film_->aspect());
         spp_ = json::value<long long>(prop, "spp");
     }
 
-    virtual void render() const override {
-		scene_->require_primitive();
-		scene_->require_accel();
-		scene_->require_camera();
-
-        const auto [w, h] = film_->size();
-        parallel::foreach(w*h, [&](long long index, int threadId) -> void {
+    virtual Json render() const override {
+        const auto size = film_->size();
+        parallel::foreach(size.w*size.h, [&](long long index, int threadId) -> void {
             thread_local Rng rng(rng_seed_ + threadId);
-            const int x = int(index % w);
-            const int y = int(index / w);
-            const auto ray = scene_->primary_ray({(x+.5_f)/w, (y+.5_f)/h}, film_->aspect_ratio());
+            const int x = int(index % size.w);
+            const int y = int(index / size.w);
+            const auto ray = path::primary_ray(scene_, {(x+.5_f)/size.w, (y+.5_f)/size.h});
             const auto hit = scene_->intersect(ray);
             if (!hit) {
                 return;
             }
             auto V = 0_f;
             for (long long i = 0; i < spp_; i++) {
-                const auto [n, u, v] = hit->geom.orthonormal_basis(-ray.d);
-                const auto d = math::sample_cosine_weighted(rng);
+                const auto [n, u, v] = hit->geom.orthonormal_basis_twosided(-ray.d);
+                const auto d = math::sample_cosine_weighted(rng.next<Vec2>());
                 V += scene_->intersect({hit->geom.p, u*d.x+v*d.y+n*d.z}, Eps, .2_f) ? 0_f : 1_f;
             }
             V /= spp_;
             film_->set_pixel(x, y, Vec3(V));
         });
+        return {};
     }
 };
 
