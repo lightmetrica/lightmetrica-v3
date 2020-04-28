c26036434d53716276aa244ac86da83d505bbf25
Luvideria <patafikss@protonmail.com>
2020-04-29 01:05:11 +0200

    remove rng in volume_multi, cosmetic changes

--- src/volume/volume_gaussian.cpp
+++ src/volume/volume_gaussian.cpp
@@ -1,6 +1,6 @@
 /*
-	Lightmetrica - Copyright (c) 2019 Hisanari Otsu
-	Distributed under MIT license. See LICENSE file for details.
+    Lightmetrica - Copyright (c) 2019 Hisanari Otsu
+    Distributed under MIT license. See LICENSE file for details.
 */
 
 #include <pch.h>
@@ -21,75 +21,75 @@ LM_NAMESPACE_BEGIN(LM_NAMESPACE)
     :param Vec3 sigma: Gaussian parameter of standard deviation for x,y,z axis
 \endrst
 */
-class Volume_Gaussian : public Volume {
+class Volume_Gaussian : public Volume{
 private:
-	Bound bound_;// Computed from 0.001% of the max of gaussian, then times two
-	std::optional<Vec3> color_;
-	std::optional<Float> scalar_;
-	Vec3 pos_;
-	Vec3 sigma_;
+    Bound bound_; // Computed from 0.001% of the max of gaussian, then times two
+    std::optional<Vec3> color_;
+    std::optional<Float> scalar_;
+    Vec3 pos_;
+    Vec3 sigma_;
 
 public:
-	LM_SERIALIZE_IMPL(ar) {
-		ar(bound_);
-	}
-
-public:
-	virtual void construct(const Json& prop) override {
-		color_ = json::value_or_none<Vec3>(prop, "color");
-		scalar_ = json::value_or_none<Float>(prop, "scalar");
-		pos_ = json::value<Vec3>(prop, "pos", Vec3(.0_f, .0_f, .0_f));
-		sigma_ = json::value<Vec3>(prop, "sigma", Vec3(1._f, 1._f, 1._f));
-
-		if ( !color_ && !scalar_ ) {
-			LM_THROW_EXCEPTION(Error::InvalidArgument,
-				"Either 'color' or 'scalar' property is necessary.");
-		}
-
-		bound_ = computeBound(sigma_);
-	}
-
-    Bound computeBound(const Vec3& sigma) const {
-		auto compAC=[&](Float s) -> Float {
-			//the first 2._f is empirical: when scalar is large, Eps is not enough
-			return 2._f*sqrt(-2._f*s*std::log(Eps));};
-		Float ac = std::max( std::max(compAC(sigma.x),compAC(sigma.y)),compAC(sigma.z) );
-		Bound b;
-		b.max = pos_+ac;
-		b.min = pos_-ac;
-
-		LM_DEBUG("min bound: {}, {}, {}", b.min.x, b.min.y, b.min.z);
-		LM_DEBUG("max bound: {}, {}, {}", b.max.x, b.max.y, b.max.z);
-		return b;
-	}
-	
-	virtual Bound bound() const override {
-		return bound_;
-	}
-
-	virtual bool has_scalar() const override {
-		return scalar_.has_value();
-	}
-
-	virtual Float max_scalar() const override {
-		return *scalar_;
-	}
-
-    Float gaussian(const Vec3& p, const Float max_v, const Vec3& s) const{
-        return max_v*exp(- 0.5_f*( (p.x*p.x)/(s.x*s.x) + (p.y*p.y)/(s.y*s.y) + (p.z*p.z)/(s.z*s.z) ) );
+    LM_SERIALIZE_IMPL(ar){
+        ar(bound_, pos_, sigma_, scalar_, color_);
     }
 
-	virtual Float eval_scalar(Vec3 p) const override {
-		return gaussian(pos_-p, max_scalar(), sigma_);
-	}
+public:
+    virtual void construct(const Json &prop) override{
+        color_ = json::value_or_none<Vec3>(prop, "color");
+        scalar_ = json::value_or_none<Float>(prop, "scalar");
+        pos_ = json::value<Vec3>(prop, "pos", Vec3(.0_f, .0_f, .0_f));
+        sigma_ = json::value<Vec3>(prop, "sigma", Vec3(1._f, 1._f, 1._f));
 
-	virtual bool has_color() const override {
-		return color_.has_value();
-	}
+        if (!color_ && !scalar_)
+            LM_THROW_EXCEPTION(Error::InvalidArgument,
+                               "Either 'color' or 'scalar' property is necessary.");
+        
+        bound_ = computeBound(sigma_);
+    }
 
-	virtual Vec3 eval_color(Vec3) const override {
-		return *color_;
-	}
+    Bound computeBound(const Vec3 &sigma) const {
+        auto compAC = [&](Float s) -> Float {
+            //the first 2._f is empirical: when scalar is large, Eps is not enough
+            return 2._f*sqrt(-2._f*s*std::log(Eps)); };
+        Float ac = std::max(std::max(compAC(sigma.x), compAC(sigma.y)), compAC(sigma.z));
+        Bound b;
+        b.max = pos_ + ac;
+        b.min = pos_ - ac;
+
+        LM_DEBUG("min bound: {}, {}, {}", b.min.x, b.min.y, b.min.z);
+        LM_DEBUG("max bound: {}, {}, {}", b.max.x, b.max.y, b.max.z);
+        return b;
+    }
+
+    virtual Bound bound() const override{
+        return bound_;
+    }
+
+    virtual bool has_scalar() const override{
+        return scalar_.has_value();
+    }
+
+    virtual Float max_scalar() const override{
+        return *scalar_;
+    }
+
+    // Compute 3D gaussian value
+    virtual Float eval_scalar(Vec3 p) const override {
+        return [&](const Vec3 p, const Float max_v, const Vec3 &s)->Float{
+            return max_v * exp(-0.5_f * ((p.x * p.x) / (s.x * s.x) +
+                                         (p.y * p.y) / (s.y * s.y) +
+                                         (p.z * p.z) / (s.z * s.z)));
+            }(pos_-p, max_scalar(), sigma_);
+    }
+
+    virtual bool has_color() const override {
+        return color_.has_value();
+    }
+
+    virtual Vec3 eval_color(Vec3) const override {
+        return *color_;
+    }
 };
 
 LM_COMP_REG_IMPL(Volume_Gaussian, "volume::gaussian");
--- src/volume/volume_sphere.cpp
+++ src/volume/volume_sphere.cpp
@@ -1,6 +1,6 @@
 /*
-	Lightmetrica - Copyright (c) 2019 Hisanari Otsu
-	Distributed under MIT license. See LICENSE file for details.
+    Lightmetrica - Copyright (c) 2019 Hisanari Otsu
+    Distributed under MIT license. See LICENSE file for details.
 */
 
 #include <pch.h>
@@ -8,7 +8,6 @@
 #include <lm/volume.h>
 
 LM_NAMESPACE_BEGIN(LM_NAMESPACE)
-#define SQ3 1.73205080757
 /*
 \rst
 .. function:: volume::sphere
@@ -17,71 +16,76 @@ LM_NAMESPACE_BEGIN(LM_NAMESPACE)
 
     :param color color: Stored color value.
     :param float scalar: Stored max scalar value.
-    :param Vec3 pos: Center position of the volume, defaults to 0, 0 ,0
+    :param Vec3 pos: Center position of the volume, defaults to [0,0,0]
     :param Float radius_: Sphere radius, defaults to 1
 \endrst
 */
 class Volume_Sphere : public Volume {
 private:
-	Bound bound_; // AABB of the sphere
-	std::optional<Vec3> color_;
-	std::optional<Float> scalar_;
-	Vec3 pos_;
-	Float radius_;
+    Bound bound_; // AABB of the sphere
+    std::optional<Vec3> color_;
+    std::optional<Float> scalar_;
+    Vec3 pos_;
+    Float radius_;
 
 public:
-	LM_SERIALIZE_IMPL(ar) {
-		ar(bound_);
-	}
-
-public:
-	virtual void construct(const Json& prop) override {
-		color_ = json::value_or_none<Vec3>(prop, "color");
-		pos_ = json::value<Vec3>(prop, "pos", Vec3(.0_f,.0_f,.0_f));
-		scalar_ = json::value_or_none<Float>(prop, "scalar");
-		radius_ = json::value<Float>(prop, "radius", 1._f);
-		if (!color_ && !scalar_) {
-			LM_THROW_EXCEPTION(Error::InvalidArgument,
-				"Either 'color' or 'scalar' property is necessary.");
-		}
-computeBound();
-	}
-
-    void computeBound(){
-        bound_.max = pos_+SQ3*radius_+Eps;
-        bound_.min = pos_-SQ3*radius_-Eps;
-
-        LM_DEBUG("min bound: {}, {}, {}",bound_.min.x,bound_.min.y,bound_.min.z);
-        LM_DEBUG("max bound: {}, {}, {}",bound_.max.x,bound_.max.y,bound_.max.z);
+    LM_SERIALIZE_IMPL(ar) {
+        ar(bound_, pos_, radius_, color_, scalar_);
     }
-	
-	virtual Bound bound() const override {
-		return bound_;
-	}
 
-	virtual bool has_scalar() const override {
-		return scalar_.has_value();
-	}
+public:
+    virtual void construct(const Json &prop) override {
+        color_ = json::value_or_none<Vec3>(prop, "color");
+        pos_ = json::value<Vec3>(prop, "pos", Vec3(.0_f, .0_f, .0_f));
+        scalar_ = json::value_or_none<Float>(prop, "scalar");
+        radius_ = json::value<Float>(prop, "radius", 1._f);
+        if (!color_ && !scalar_){
+            LM_THROW_EXCEPTION(Error::InvalidArgument,
+                               "Either 'color' or 'scalar' property is necessary.");
+        }
+        computeBound();
+    }
 
-	virtual Float max_scalar() const override {
-		return *scalar_;
-	}
+    void computeBound() {
+        // square root of 3 for box computation from sphere center
+        // Eps ensures the sphere is entirely contained
+        const Float sq3 = 1.73205080757_f;
+        bound_.max = pos_ + sq3 * radius_ + Eps;
+        bound_.min = pos_ - sq3 * radius_ - Eps;
 
-	bool inSphere(const Vec3& p, const Float r) const{
-		return ( glm::length(p) < r )? true:false;
-	}
+        LM_DEBUG("min bound: {}, {}, {}", bound_.min.x, bound_.min.y, bound_.min.z);
+        LM_DEBUG("max bound: {}, {}, {}", bound_.max.x, bound_.max.y, bound_.max.z);
+    }
 
-	virtual Float eval_scalar(Vec3 p) const override {
-		return max_scalar()*inSphere(pos_-p, radius_);
-	}
+    virtual Bound bound() const override {
+        return bound_;
+    }
 
-	virtual bool has_color() const override {
-		return color_.has_value();
-	}
+    virtual bool has_scalar() const override {
+        
+        return scalar_.has_value();
+    }
 
-	virtual Vec3 eval_color(Vec3) const override {
-		return *color_;
-	}
+    virtual Float max_scalar() const override {
+        
+        return *scalar_;
+    }
+
+    bool inSphere(const Vec3 &p, const Float r) const {
+        return (glm::length(p) < r) ? true : false;
+    }
+
+    virtual Float eval_scalar(Vec3 p) const override {
+        return max_scalar() * inSphere(pos_ - p, radius_);
+    }
+
+    virtual bool has_color() const override {
+        return color_.has_value();
+    }
+
+    virtual Vec3 eval_color(Vec3) const override {
+        return *color_;
+    }
 };
 
 LM_COMP_REG_IMPL(Volume_Sphere, "volume::sphere");
